66565170e4c49135d4a3798af9deda5a5f5b3f7b
Danny <dannygott98@gmail.com>
2019-09-22 12:34:30 -0400

    Implemented turning on arduino side

--- Serial/Serial.ino
+++ Serial/Serial.ino
@@ -1,124 +1,139 @@
 #include <Servo.h>
-Servo myservo;  // create servo object to control a servo
 
+// Servo objects
+Servo throttleServo;
+Servo turnSpeedController;
+// Encoder Pins
 int encoder0PinA = 32;
-int encoder0PinB = 33; 
+int encoder0PinB = 33;
 int encoder0Pos = 0;
-
+// Shifting Pins
 int shiftupPin = 24;
 int shiftupStop = 25;
 int shiftdownPin = 23;
 int shiftdownStop = 22;
+// Potentiometer pin for Turning
+int turnPotPin = 0;
+// Various value storage
 int encoder0PinALast = LOW;
 int servoPoz = 0;
-
 int n = LOW;
 int shiftVal = 0;
 int prevShiftVal = 0;
 int shiftPoz = 1;
-
 int previousMillis = -1;
 int tBetweenPulse = 0;
 int previousEncoder0Pos = 0;
+int turnValue = 180;
+// Indicates status of entire system. 0 = Stopped, 1 = Running, 2 = Error
+int systemStatus = 0;
 
 void setup() {
-  pinMode (encoder0PinA, INPUT_PULLUP); //Dont forget to change
-  pinMode (encoder0PinB, INPUT_PULLUP);
-  pinMode (shiftupPin, OUTPUT);
-  pinMode (shiftdownPin, OUTPUT);
+  pinMode(encoder0PinA, INPUT_PULLUP); // Dont forget to change
+  pinMode(encoder0PinB, INPUT_PULLUP);
+  pinMode(shiftupPin, OUTPUT);
+  pinMode(shiftdownPin, OUTPUT);
   Serial.begin(2000000);
-  myservo.attach(40);
+  throttleServo.attach(40);
+  turnSpeedController.attach(41);
   Serial.setTimeout(10);
-  
 }
 
 void loop() {
-    
-    if (Serial.available()){
-     // If anything comes in Serial (USB),
-     servoPoz = Serial.readStringUntil(',').toInt();
-     shiftVal = Serial.readStringUntil(',').toInt();
+  // If anything comes in Serial (USB)
+  if (Serial.available()) {
 
-     Serial.println(encoder0Pos);
-     Serial.println(shiftPoz);
-     Serial.println(encoderStopHandler(encoder0Pos));
-    }
+    servoPoz = Serial.readStringUntil(',').toInt();
+    shiftVal = Serial.readStringUntil(',').toInt();
+    turnValue = Serial.readStringUntil(',').toInt();
 
-    handleShift(shiftVal);    
-    handleEncoder(digitalRead(encoder0PinA));
-    myservo.write(servoPoz);
+    Serial.println(encoder0Pos);
+    Serial.println(shiftPoz);
+    Serial.println(handleEncoderStop(encoder0Pos));
+    // Serial.println(systemStatus); //TODO: Implement Later
   }
+  handleTurn(turnValue);
+  handleShift(shiftVal);
+  handleEncoder(digitalRead(encoder0PinA));
+  throttleServo.write(servoPoz);
+}
 
+int handleEncoderStop(int encoderVal) {
 
-  int encoderStopHandler(int encoderVal){
-    
-    if (previousEncoder0Pos == encoderVal){
-      tBetweenPulse = 0;
-    }
-      previousEncoder0Pos = encoder0Pos;
-      return tBetweenPulse;
-    }
+  if (previousEncoder0Pos == encoderVal) {
+    tBetweenPulse = 0;
+  }
+  previousEncoder0Pos = encoder0Pos;
+  return tBetweenPulse;
+}
 
-void handleShift(int shiftValue){
+void handleShift(int shiftValue) {
   switch (shiftValue) {
-    case 0:
-      if(prevShiftVal == 1){
-        shiftPoz--;
-        prevShiftVal = 0;
-      }else if(prevShiftVal == 2){
-        shiftPoz++;
-        prevShiftVal = 0;
-      }
-      digitalWrite(shiftupPin, LOW);
-      digitalWrite(shiftdownPin, LOW);
-      digitalWrite(shiftdownStop, HIGH);
-      digitalWrite(shiftupStop, HIGH);
-      break;
-    case 1:
-      if(shiftPoz > 0){
-        digitalWrite(shiftupStop, LOW);
-        digitalWrite(shiftdownPin, HIGH);
-        prevShiftVal = shiftValue;
-      }
-      break;
-    
-    case 2:
-      if(shiftPoz < 3){
-        digitalWrite(shiftupStop, LOW);
-        digitalWrite(shiftupPin, HIGH);
-        prevShiftVal = shiftValue;
-      }
-      break;
-    default:
-      break;
-  } 
+  case 0:
+    if (prevShiftVal == 1) {
+      shiftPoz--;
+      prevShiftVal = 0;
+    } else if (prevShiftVal == 2) {
+      shiftPoz++;
+      prevShiftVal = 0;
+    }
+    digitalWrite(shiftupPin, LOW);
+    digitalWrite(shiftdownPin, LOW);
+    digitalWrite(shiftdownStop, HIGH);
+    digitalWrite(shiftupStop, HIGH);
+    break;
+  case 1:
+    if (shiftPoz > 0) {
+      digitalWrite(shiftupStop, LOW);
+      digitalWrite(shiftdownPin, HIGH);
+      prevShiftVal = shiftValue;
+    }
+    break;
+
+  case 2:
+    if (shiftPoz < 3) {
+      digitalWrite(shiftupStop, LOW);
+      digitalWrite(shiftupPin, HIGH);
+      prevShiftVal = shiftValue;
+    }
+    break;
+  default:
+    break;
+  }
 }
 
-void handleEncoder(int encoderPinVal){
+void handleEncoder(int encoderPinVal) {
   if ((encoder0PinALast == LOW) && (encoderPinVal == HIGH)) {
-      
-      if (digitalRead(encoder0PinB) == LOW) {
-        if (previousMillis == -1){
-          previousMillis = millis();
-        }
-        else{
-          tBetweenPulse = millis() - previousMillis;
-          previousMillis = millis();
-        }
-        
-        encoder0Pos--;
+
+    if (digitalRead(encoder0PinB) == LOW) {
+      if (previousMillis == -1) {
+        previousMillis = millis();
       } else {
-         if (previousMillis == -1){
-          previousMillis = millis();
-        }
-        else{
-          tBetweenPulse = millis() - previousMillis;
-          previousMillis = millis();
-        }
-        encoder0Pos++;
-        
+        tBetweenPulse = millis() - previousMillis;
+        previousMillis = millis();
       }
+
+      encoder0Pos--;
+    } else {
+      if (previousMillis == -1) {
+        previousMillis = millis();
+      } else {
+        tBetweenPulse = millis() - previousMillis;
+        previousMillis = millis();
+      }
+      encoder0Pos++;
     }
-   encoder0PinALast = encoderPinVal;
+  }
+  encoder0PinALast = encoderPinVal;
 }
-  
+
+void handleTurn(int poz) {
+  // reads the value of the potentiometer (value between 0 and 1023)
+  int val = analogRead(potpin);
+  val = map(val, 0, 1023, 0, 360); // Map pot values to make sense to servo
+
+  // If the current pot position is not equal to the turn value, turn the motor
+  if (poz != val) {
+    turnSpeedController.write(val);
+  }
+}
